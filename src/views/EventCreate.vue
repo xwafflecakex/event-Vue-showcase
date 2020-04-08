4dc6178b1efd61863740cefe86f558ab18173493
xwafflecakex <nyevtukh@gmail.com>
2020-04-08 15:16:02 -0700

    Added TS config, and then a TODO list

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,30 +1,44 @@
 <template>
   <div>
-        <h1>Create an Event, {{ user.name }}</h1>
-        <p>This was created by {{ user.name }}</p>
-        <p>There are {{ catLength }} categories</p>
-        <p>The event: {{ getEventById(2) }} </p>
+    <h1>Create an Event, {{ user.name }}</h1>
+    <p>This was created by {{ user.name }}</p>
+    <p>Number of active TODO's: {{ activeTodosCount }}</p>
+    <p>The list of TODOS:</p>
+    <ul>
+      <li
+        v-for="todo in todos"
+        :key='todo'
+      >
+        {{ todo }}
+      </li>
+    </ul>
+    <p>There are {{ catLength }} categories</p>
+    <p>The event: {{ getEventById(2) }}</p>
 
-        <ul>
-          <li v-for="cat in categories" :key='cat'>{{ cat }}</li>
-        </ul>
+    <ul>
+      <li
+        v-for="cat in categories"
+        :key='cat'
+      >{{ cat }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-import { mapState, mapGetters } from 'vuex'
-  export default {
-    computed: {
-      ...mapGetters(['getEventById', 'catLength']),
-      // the unmapped getters.
-      // catLength() {
-      //   return this.$store.getters.catLength
-      // },
-      // getEvent() {
-      //   return this.$store.getters.getEventById
-      // }
-      // the object spread operator
-      ...mapState(['user','categories']
+import { mapState, mapGetters } from "vuex";
+export default {
+  computed: {
+    ...mapGetters(["getEventById", "catLength", "activeTodosCount"]),
+    // the unmapped getters.
+    // catLength() {
+    //   return this.$store.getters.catLength
+    // },
+    // getEvent() {
+    //   return this.$store.getters.getEventById
+    // }
+    // the object spread operator
+    ...mapState(
+      ["user", "categories", "todos"]
       // user: 'user'
       // --The above is a simplified version of state declaration for the below.--
       // userName: state => state.user.name,
@@ -32,9 +46,8 @@ import { mapState, mapGetters } from 'vuex'
       // categories: state => state.categories
     )
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
