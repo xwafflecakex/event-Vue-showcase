55dafdc89036952c32a3851c83c371d88d111da0
xwafflecakex <nyevtukh@gmail.com>
2020-04-19 16:10:19 -0700

    Simple loading display with nprogress

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
+import NProgress from 'nprogress'
 
+// Single instance of Axios.
 const apiClient = axios.create({
   baseURL: `http://localhost:3000`,
   withCredentials: false, // This is the default
@@ -8,6 +10,25 @@ const apiClient = axios.create({
     'Content-Type': 'application/json'
   }
 })
+
+// This solution is not so good for multiple API calls. Need a LOADING Vuex module to hold the state when LOADING.
+// Templates are also rednered before the API calls are returned.
+/* On some webpages, it’s not a great user experience to see the page before the data is populated in it reactively. 
+You’ll notice when we load up /event/5 (an event that we have to pull from the API) 
+that the page loads empty before the data flashes onto the screen 1.5 seconds later.
+his is NOT an optimal user experience since the user sees the page broken before they see it working.
+*/
+// Using interceptors for the API Calls we will send the config with the start progress bar command
+apiClient.interceptors.request.use(config => { // Called on request
+  NProgress.start()
+  return config
+})
+// No we till say when to finish the progress bar with the responce of the server API Call back
+apiClient.interceptors.response.use(response => { // Called on response
+  NProgress.done()
+  return response
+})
+
 
 export default {
   getEvents(perPage, page) {
